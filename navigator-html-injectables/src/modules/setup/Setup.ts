cf6e52f3ed3d9b64912c565ef8d6e558addbb296
Henry <chocolatkey@gmail.com>
2023-05-22 00:51:42 -0700

    remove console log

--- navigator-html-injectables/src/modules/setup/Setup.ts
+++ navigator-html-injectables/src/modules/setup/Setup.ts
@@ -31,7 +31,6 @@ export abstract class Setup extends Module {
                     break;
                 case 1:
                     const ev = x[1];
-                    console.log("Blocked event:", ev);
                     window.removeEventListener(ev.type, wnd._readium_eventBlocker, true);
                     const evt = new Event(ev.type, {
                         bubbles: ev.bubbles,
