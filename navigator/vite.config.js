32ed06135bf780efd55ec9d929946854d1f714ef
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2024-09-10 08:30:51 +0200

    Output static assets in navigator
    Vite’s Library mode can’t export static assets and defaults to Base64. So we currently need a plugin to handle that: https://github.com/vitejs/vite/issues/3295
    
    Note: to do things properly, we should also bundle the fonts’ licenses.

--- navigator/vite.config.js
+++ navigator/vite.config.js
@@ -1,7 +1,14 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import libAssetsPlugin from "@laynezh/vite-plugin-lib-assets";
 
 export default defineConfig({
+  plugins: [
+    libAssetsPlugin({
+      extensions: [".jpg", ".png", ".svg", ".otf", ".ttf", ".woff", ".woff2"],
+      name: "[name].[ext]"
+    })
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
