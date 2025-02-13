45c83a4052f86864c412dcf4c49bfcf651fd7e68
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2025-02-13 16:14:24 +0100

    EpubSettings class

---
+++ navigator/src/epub/preferences/EpubSettings.ts
@@ -0,0 +1,96 @@
+import { ConfigurableSettings } from "../../preferences/Configurable";
+import { TextAlignment, Theme } from "../../preferences/Types";
+
+// Expose everything available in Preferences ATM
+export interface IEpubSettings {
+  backgroundColor?: string | null;
+  columnCount?: number | null;
+  fontFamily?: string | null;
+  fontSize?: number | null;
+  fontOpticalSizing?: number | null;
+  fontWeight?: number | null;
+  fontWidth?: number | null;
+  hyphens?: boolean | null;
+  imageFilters?: any | null;
+  letterSpacing?: number | null;
+  ligatures?: boolean | null;
+  lineHeight?: number | null;
+  lineLength?: number | null;
+  linkColor?: string | null;
+  noRuby?: boolean | null;
+  pageMargins?: number | null;
+  paragraphIndent?: number | null;
+  paragraphSpacing?: number | null;
+  publisherStyles?: boolean | null;
+  scroll?: boolean | null;
+  selectionBackgroundColor?: string | null;
+  selectionTextColor?: string | null;
+  textAlign?: TextAlignment | null;
+  textColor?: string | null;
+  textNormalization?: boolean | null;
+  theme?: Theme | null;
+  visitedLinkColor?: string | null;
+  wordSpacing?: number | null;
+}
+
+export class EpubSettings implements ConfigurableSettings {
+  backgroundColor: string | null;
+  columnCount: number | null;
+  fontFamily: string | null;
+  fontSize: number | null;
+  fontOpticalSizing: number | null;
+  fontWeight: number | null;
+  fontWidth: number | null;
+  hyphens: boolean | null;
+  imageFilters: any | null;
+  letterSpacing: number | null;
+  ligatures: boolean | null;
+  lineHeight: number | null;
+  lineLength: number | null;
+  linkColor: string | null;
+  noRuby: boolean | null;
+  pageMargins: number | null;
+  paragraphIndent: number | null;
+  paragraphSpacing: number | null;
+  publisherStyles: boolean | null;
+  scroll: boolean | null;
+  selectionBackgroundColor: string | null;
+  selectionTextColor: string | null;
+  textAlign: TextAlignment | null;
+  textColor: string | null;
+  textNormalization: boolean | null;
+  theme: Theme | null;
+  visitedLinkColor: string | null;
+  wordSpacing: number | null;
+
+  constructor(settings: IEpubSettings) {
+    this.backgroundColor = settings.backgroundColor || null;
+    this.columnCount = settings.columnCount || null;
+    this.fontFamily = settings.fontFamily || null;
+    this.fontSize = settings.fontSize || null;
+    this.fontOpticalSizing = settings.fontOpticalSizing || null;
+    this.fontWeight = settings.fontWeight || null;
+    this.fontWidth = settings.fontWidth || null;
+    this.hyphens = settings.hyphens || null;
+    this.imageFilters = settings.imageFilters || null;
+    this.letterSpacing = settings.letterSpacing || null;
+    this.ligatures = settings.ligatures || null;
+    this.lineHeight = settings.lineHeight || null;
+    this.lineLength = settings.lineLength || null;
+    this.linkColor = settings.linkColor || null;
+    this.noRuby = settings.noRuby || null;
+    this.pageMargins = settings.pageMargins || null;
+    this.paragraphIndent = settings.paragraphIndent || null;
+    this.paragraphSpacing = settings.paragraphSpacing || null;
+    this.publisherStyles = settings.publisherStyles || null;
+    this.scroll = settings.scroll || null;
+    this.selectionBackgroundColor = settings.selectionBackgroundColor || null;
+    this.selectionTextColor = settings.selectionTextColor || null;
+    this.textAlign = settings.textAlign || null;
+    this.textColor = settings.textColor || null;
+    this.textNormalization = settings.textNormalization || null;
+    this.theme = settings.theme || null;
+    this.visitedLinkColor = settings.visitedLinkColor || null;
+    this.wordSpacing = settings.wordSpacing || null;
+  }
+}
