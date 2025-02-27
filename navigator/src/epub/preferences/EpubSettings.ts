25468c57e381b2397b53db26a332fd2baa9883f9
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2025-02-27 10:21:53 +0100

    Correct UserSettings constructor

--- navigator/src/epub/preferences/EpubSettings.ts
+++ navigator/src/epub/preferences/EpubSettings.ts
@@ -3,7 +3,6 @@ import { TextAlignment, Theme } from "../../preferences/Types";
 import { EpubDefaults } from "./EpubDefaults";
 import { EpubPreferences } from "./EpubPreferences";
 
-// Expose everything available in Preferences except blend and gaiji filters ATM
 export interface IEpubSettings {
   backgroundColor?: string | null,
   blendFilter?: boolean | null,
@@ -71,32 +70,54 @@ export class EpubSettings implements ConfigurableSettings {
 
   constructor(preferences: EpubPreferences, defaults: EpubDefaults) {
     this.backgroundColor = preferences.backgroundColor || defaults.backgroundColor || null;
-    this.blendFilter = preferences.blendFilter || defaults.blendFilter || null;
+    this.blendFilter = typeof preferences.blendFilter === "boolean" 
+      ? preferences.blendFilter 
+      : defaults.blendFilter || null;
     this.columnCount = preferences.columnCount || defaults.columnCount || null;
-    this.darkenFilter = preferences.darkenFilter || defaults.darkenFilter || null;
+    this.darkenFilter = typeof preferences.darkenFilter === "boolean" 
+      ? preferences.darkenFilter 
+      : defaults.darkenFilter || null;
     this.fontFamily = preferences.fontFamily || defaults.fontFamily || null;
     this.fontSize = preferences.fontSize || defaults.fontSize || null;
-    this.fontOpticalSizing = preferences.fontOpticalSizing || defaults.fontOpticalSizing || null;
+    this.fontOpticalSizing = typeof preferences.fontOpticalSizing === "boolean" 
+      ? preferences.fontOpticalSizing 
+      : defaults.fontOpticalSizing || null;
     this.fontWeight = preferences.fontWeight || defaults.fontWeight || null;
     this.fontWidth = preferences.fontWidth || defaults.fontWidth || null;
-    this.hyphens = preferences.hyphens || defaults.hyphens || null;
-    this.invertFilter = preferences.invertFilter || defaults.invertFilter || null;
-    this.invertGaijiFilter = preferences.invertGaijiFilter || defaults.invertGaijiFilter || null;
+    this.hyphens = typeof preferences.hyphens === "boolean" 
+      ? preferences.hyphens 
+      : defaults.hyphens || null;
+    this.invertFilter = typeof preferences.invertFilter === "boolean" 
+      ? preferences.invertFilter 
+      : defaults.invertFilter || null;
+    this.invertGaijiFilter = typeof preferences.invertGaijiFilter === "boolean" 
+      ? preferences.invertGaijiFilter 
+      : defaults.invertGaijiFilter || null;
     this.letterSpacing = preferences.letterSpacing || defaults.letterSpacing || null;
-    this.ligatures = preferences.ligatures || defaults.ligatures || null;
+    this.ligatures = typeof preferences.ligatures === "boolean"
+      ? preferences.ligatures 
+      : defaults.ligatures || null;
     this.lineHeight = preferences.lineHeight || defaults.lineHeight || null;
     this.lineLength = preferences.lineLength || defaults.lineLength || null;
     this.linkColor = preferences.linkColor || defaults.linkColor || null;
-    this.noRuby = preferences.noRuby || defaults.noRuby || null;
+    this.noRuby = typeof preferences.noRuby === "boolean" 
+      ? preferences.noRuby 
+      : defaults.noRuby || null;
     this.paragraphIndent = preferences.paragraphIndent || defaults.paragraphIndent || null;
     this.paragraphSpacing = preferences.paragraphSpacing || defaults.paragraphSpacing || null;
-    this.publisherStyles = preferences.publisherStyles || defaults.publisherStyles || null;
-    this.scroll = preferences.scroll || defaults.scroll || null;
+    this.publisherStyles = typeof preferences.publisherStyles === "boolean" 
+      ? preferences.publisherStyles 
+      : defaults.publisherStyles || null;
+    this.scroll = typeof preferences.scroll === "boolean" 
+      ? preferences.scroll 
+      : defaults.scroll || null;
     this.selectionBackgroundColor = preferences.selectionBackgroundColor || defaults.selectionBackgroundColor || null;
     this.selectionTextColor = preferences.selectionTextColor || defaults.selectionTextColor || null;
     this.textAlign = preferences.textAlign || defaults.textAlign || null;
     this.textColor = preferences.textColor || defaults.textColor || null;
-    this.textNormalization = preferences.textNormalization || defaults.textNormalization || null;
+    this.textNormalization = typeof preferences.textNormalization === "boolean" 
+      ? preferences.textNormalization 
+      : defaults.textNormalization || null;
     this.theme = preferences.theme || defaults.theme || null;
     this.visitedColor = preferences.visitedColor || defaults.visitedColor || null;
     this.wordSpacing = preferences.wordSpacing || defaults.wordSpacing || null;
