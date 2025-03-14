8fe11c6e517645d1880aa02bb827461a8d7138c1
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2025-03-14 15:24:38 +0100

    Add font-family to relayout condition

--- navigator/src/epub/EpubNavigator.ts
+++ navigator/src/epub/EpubNavigator.ts
@@ -175,8 +175,10 @@ export class EpubNavigator extends VisualNavigator implements Configurable<Confi
         if (this.layout === EPUBLayout.fixed) {
             this.handleFXLPrefs(oldSettings, this._settings);
         } else {
+            // TODO: check what’s necessary
             const relayout = (
                 (oldSettings.fontSize !== this._settings.fontSize) || 
+                (oldSettings.fontFamily !== this._settings.fontFamily) ||
                 (oldSettings.minimalLineLength !== this._settings.minimalLineLength) ||
                 (oldSettings.optimalLineLength !== this._settings.optimalLineLength) ||
                 (oldSettings.maximalLineLength !== this._settings.maximalLineLength) ||
