25f624b9013c0a573069a78effc34eab4e7eadad
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2025-06-10 11:05:56 +0200

    Bulletproof appendVirtualColumnIfNeeded
    This brings the helper in line with kotlin-toolkit after issues were discovered with rounding on Android.

--- navigator-html-injectables/src/helpers/document.ts
+++ navigator-html-injectables/src/helpers/document.ts
@@ -4,7 +4,7 @@ export function isRTL(wnd: ReadiumWindow): boolean {
     return wnd.document.body.dir.toLowerCase() === "rtl";
 }
 
-export function getColumnCountPerScreen(wnd: ReadiumWindow) {
+export function getColumnCountPerScreen(wnd: ReadiumWindow): number {
     return parseInt(
         wnd.getComputedStyle(
             wnd.document.documentElement
@@ -18,7 +18,7 @@ export function getColumnCountPerScreen(wnd: ReadiumWindow) {
 *
 * @param el - The element to measure.
 */
-export function getContentHeight(el: Element) {
+export function getContentHeight(el: Element): number {
  const cStyle = getComputedStyle(el);
  const paddingTop = parseFloat(cStyle.paddingTop || "0");
  const paddingBottom = parseFloat(cStyle.paddingBottom || "0");
@@ -31,7 +31,14 @@ export function getContentHeight(el: Element) {
  * Otherwise it causes snapping and page turning issues. 
  * To fix this, we insert and remove blank virtual columns at the end of the resource.
  */
-export function appendVirtualColumnIfNeeded(wnd: ReadiumWindow) {
+export function appendVirtualColumnIfNeeded(wnd: ReadiumWindow): boolean {
+    const colCountPerScreen = getColumnCountPerScreen(wnd);
+
+    if (!colCountPerScreen) {
+        // This has been triggered while in scroll mode
+        return false;
+    }
+
     const virtualCols = wnd.document.querySelectorAll("div[id^='readium-virtual-page']");
     // Remove first so that we don’t end up with an incorrect scrollWidth
     // Even when removing their width we risk having an incorrect scrollWidth
@@ -39,11 +46,19 @@ export function appendVirtualColumnIfNeeded(wnd: ReadiumWindow) {
     for (const virtualCol of virtualCols) {
         virtualCol.remove();
     }
-    const colCount = getColumnCountPerScreen(wnd);
+    const virtualColsCount = virtualCols.length
+
     const documentWidth = wnd.document.scrollingElement!.scrollWidth;
-    const neededColCount = Math.ceil(documentWidth / wnd.innerWidth) * colCount;
-    const totalColCount = Math.round((documentWidth / wnd.innerWidth) * colCount);
-    const needed = colCount === 1 ? 0 : neededColCount - totalColCount;
+    const windowWidth = wnd.visualViewport!.width
+
+    const totalColCount = Math.round((documentWidth / windowWidth) * colCountPerScreen);
+
+    const lonelyColCount = totalColCount % colCountPerScreen;
+
+    const needed = colCountPerScreen === 1 || lonelyColCount === 0
+        ? 0 
+        : colCountPerScreen - lonelyColCount;
+
     if (needed > 0) {
         for (let i = 0; i < needed; i++) {
             const virtualCol = wnd.document.createElement("div");
@@ -61,4 +76,6 @@ export function appendVirtualColumnIfNeeded(wnd: ReadiumWindow) {
             wnd.document.body.appendChild(virtualCol);
         }
     }
+
+    return virtualColsCount !== needed;
 }
