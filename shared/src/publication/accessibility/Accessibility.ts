cf3313f2a51f7fec40b2ee7db5837aa0cba5f5e7
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2025-07-07 16:51:31 +0200

    Normalise quotes in Accessibility

--- shared/src/publication/accessibility/Accessibility.ts
+++ shared/src/publication/accessibility/Accessibility.ts
@@ -176,18 +176,18 @@ export class Profile {
         return this.uri;
     }
 
-    public static readonly EPUB_A11Y_10_WCAG_20_A = new Profile("http://www.idpf.org/epub/a11y/accessibility-20170105.html#wcag-a");
-    public static readonly EPUB_A11Y_10_WCAG_20_AA = new Profile("http://www.idpf.org/epub/a11y/accessibility-20170105.html#wcag-aa");
-    public static readonly EPUB_A11Y_10_WCAG_20_AAA = new Profile("http://www.idpf.org/epub/a11y/accessibility-20170105.html#wcag-aaa");
-    public static readonly EPUB_A11Y_11_WCAG_20_A = new Profile("https://www.w3.org/TR/epub-a11y-11#wcag-2.0-a");
-    public static readonly EPUB_A11Y_11_WCAG_20_AA = new Profile("https://www.w3.org/TR/epub-a11y-11#wcag-2.0-aa");
-    public static readonly EPUB_A11Y_11_WCAG_20_AAA = new Profile("https://www.w3.org/TR/epub-a11y-11#wcag-2.0-aaa");
-    public static readonly EPUB_A11Y_11_WCAG_21_A = new Profile("https://www.w3.org/TR/epub-a11y-11#wcag-2.1-a");
-    public static readonly EPUB_A11Y_11_WCAG_21_AA = new Profile("https://www.w3.org/TR/epub-a11y-11#wcag-2.1-aa");
-    public static readonly EPUB_A11Y_11_WCAG_21_AAA = new Profile("https://www.w3.org/TR/epub-a11y-11#wcag-2.1-aaa");
-    public static readonly EPUB_A11Y_11_WCAG_22_A = new Profile("https://www.w3.org/TR/epub-a11y-11#wcag-2.2-a");
-    public static readonly EPUB_A11Y_11_WCAG_22_AA = new Profile("https://www.w3.org/TR/epub-a11y-11#wcag-2.2-aa");
-    public static readonly EPUB_A11Y_11_WCAG_22_AAA = new Profile("https://www.w3.org/TR/epub-a11y-11#wcag-2.2-aaa");
+    public static readonly EPUB_A11Y_10_WCAG_20_A = new Profile('http://www.idpf.org/epub/a11y/accessibility-20170105.html#wcag-a');
+    public static readonly EPUB_A11Y_10_WCAG_20_AA = new Profile('http://www.idpf.org/epub/a11y/accessibility-20170105.html#wcag-aa');
+    public static readonly EPUB_A11Y_10_WCAG_20_AAA = new Profile('http://www.idpf.org/epub/a11y/accessibility-20170105.html#wcag-aaa');
+    public static readonly EPUB_A11Y_11_WCAG_20_A = new Profile('https://www.w3.org/TR/epub-a11y-11#wcag-2.0-a');
+    public static readonly EPUB_A11Y_11_WCAG_20_AA = new Profile('https://www.w3.org/TR/epub-a11y-11#wcag-2.0-aa');
+    public static readonly EPUB_A11Y_11_WCAG_20_AAA = new Profile('https://www.w3.org/TR/epub-a11y-11#wcag-2.0-aaa');
+    public static readonly EPUB_A11Y_11_WCAG_21_A = new Profile('https://www.w3.org/TR/epub-a11y-11#wcag-2.1-a');
+    public static readonly EPUB_A11Y_11_WCAG_21_AA = new Profile('https://www.w3.org/TR/epub-a11y-11#wcag-2.1-aa');
+    public static readonly EPUB_A11Y_11_WCAG_21_AAA = new Profile('https://www.w3.org/TR/epub-a11y-11#wcag-2.1-aaa');
+    public static readonly EPUB_A11Y_11_WCAG_22_A = new Profile('https://www.w3.org/TR/epub-a11y-11#wcag-2.2-a');
+    public static readonly EPUB_A11Y_11_WCAG_22_AA = new Profile('https://www.w3.org/TR/epub-a11y-11#wcag-2.2-aa');
+    public static readonly EPUB_A11Y_11_WCAG_22_AAA = new Profile('https://www.w3.org/TR/epub-a11y-11#wcag-2.2-aaa');
 
     public get isWCAGLevelA(): boolean {
         return this === Profile.EPUB_A11Y_10_WCAG_20_A ||
@@ -266,22 +266,22 @@ export class AccessMode {
         return this.value;
     }
 
-    public static readonly AUDITORY = new AccessMode("auditory");
-    public static readonly CHART_ON_VISUAL = new AccessMode("chartOnVisual");
-    public static readonly CHEM_ON_VISUAL = new AccessMode("chemOnVisual");
-    public static readonly COLOR_DEPENDENT = new AccessMode("colorDependent");
-    public static readonly DIAGRAM_ON_VISUAL = new AccessMode("diagramOnVisual");
-    public static readonly MATH_ON_VISUAL = new AccessMode("mathOnVisual");
-    public static readonly MUSIC_ON_VISUAL = new AccessMode("musicOnVisual");
-    public static readonly TACTILE = new AccessMode("tactile");
-    public static readonly TEXT_ON_VISUAL = new AccessMode("textOnVisual");
-    public static readonly TEXTUAL = new AccessMode("textual");
-    public static readonly VISUAL = new AccessMode("visual");
+    public static readonly AUDITORY = new AccessMode('auditory');
+    public static readonly CHART_ON_VISUAL = new AccessMode('chartOnVisual');
+    public static readonly CHEM_ON_VISUAL = new AccessMode('chemOnVisual');
+    public static readonly COLOR_DEPENDENT = new AccessMode('colorDependent');
+    public static readonly DIAGRAM_ON_VISUAL = new AccessMode('diagramOnVisual');
+    public static readonly MATH_ON_VISUAL = new AccessMode('mathOnVisual');
+    public static readonly MUSIC_ON_VISUAL = new AccessMode('musicOnVisual');
+    public static readonly TACTILE = new AccessMode('tactile');
+    public static readonly TEXT_ON_VISUAL = new AccessMode('textOnVisual');
+    public static readonly TEXTUAL = new AccessMode('textual');
+    public static readonly VISUAL = new AccessMode('visual');
 }
 
 export class PrimaryAccessMode {
     public readonly value!: string | string[];
-    private static readonly VALID_MODES = new Set(["auditory", "tactile", "textual", "visual"]);
+    private static readonly VALID_MODES = new Set(['auditory', 'tactile', 'textual', 'visual']);
 
     constructor(value: string | string[]) {
         if (typeof value === 'string') {
@@ -331,10 +331,10 @@ export class PrimaryAccessMode {
         return this.value;
     }
 
-    public static readonly AUDITORY = new PrimaryAccessMode("auditory");
-    public static readonly TACTILE = new PrimaryAccessMode("tactile");
-    public static readonly TEXTUAL = new PrimaryAccessMode("textual");
-    public static readonly VISUAL = new PrimaryAccessMode("visual");
+    public static readonly AUDITORY = new PrimaryAccessMode('auditory');
+    public static readonly TACTILE = new PrimaryAccessMode('tactile');
+    public static readonly TEXTUAL = new PrimaryAccessMode('textual');
+    public static readonly VISUAL = new PrimaryAccessMode('visual');
 }
 
 export class Feature {
@@ -353,48 +353,48 @@ export class Feature {
         return this.value;
     }
 
-    public static readonly NONE = new Feature("none");
-    public static readonly ANNOTATIONS = new Feature("annotations");
-    public static readonly ARIA = new Feature("ARIA");
-    public static readonly INDEX = new Feature("index");
-    public static readonly PAGE_BREAK_MARKERS = new Feature("pageBreakMarkers");
-    public static readonly PAGE_NAVIGATION = new Feature("pageNavigation");
-    public static readonly PRINT_PAGE_NUMBERS = new Feature("printPageNumbers");
-    public static readonly READING_ORDER = new Feature("readingOrder");
-    public static readonly STRUCTURAL_NAVIGATION = new Feature("structuralNavigation");
-    public static readonly TABLE_OF_CONTENTS = new Feature("tableOfContents");
-    public static readonly TAGGED_PDF = new Feature("taggedPDF");
-    public static readonly ALTERNATIVE_TEXT = new Feature("alternativeText");
-    public static readonly AUDIO_DESCRIPTION = new Feature("audioDescription");
-    public static readonly CAPTIONS = new Feature("captions");
-    public static readonly CLOSED_CAPTIONS = new Feature("closedCaptions");
-    public static readonly DESCRIBED_MATH = new Feature("describedMath");
-    public static readonly LONG_DESCRIPTION = new Feature("longDescription");
-    public static readonly OPEN_CAPTIONS = new Feature("openCaptions");
-    public static readonly SIGN_LANGUAGE = new Feature("signLanguage");
-    public static readonly TRANSCRIPT = new Feature("transcript");
-    public static readonly DISPLAY_TRANSFORMABILITY = new Feature("displayTransformability");
-    public static readonly SYNCHRONIZED_AUDIO_TEXT = new Feature("synchronizedAudioText");
-    public static readonly TIMING_CONTROL = new Feature("timingControl");
-    public static readonly UNLOCKED = new Feature("unlocked");
-    public static readonly CHEM_ML = new Feature("ChemML");
-    public static readonly LATEX = new Feature("latex");
-    public static readonly LATEX_CHEMISTRY = new Feature("latex-chemistry");
-    public static readonly MATH_ML = new Feature("MathML");
-    public static readonly MATH_ML_CHEMISTRY = new Feature("MathML-chemistry");
-    public static readonly TTS_MARKUP = new Feature("ttsMarkup");
-    public static readonly HIGH_CONTRAST_AUDIO = new Feature("highContrastAudio");
-    public static readonly HIGH_CONTRAST_DISPLAY = new Feature("highContrastDisplay");
-    public static readonly LARGE_PRINT = new Feature("largePrint");
-    public static readonly BRAILLE = new Feature("braille");
-    public static readonly TACTILE_GRAPHIC = new Feature("tactileGraphic");
-    public static readonly TACTILE_OBJECT = new Feature("tactileObject");
-    public static readonly FULL_RUBY_ANNOTATIONS = new Feature("fullRubyAnnotations");
-    public static readonly HORIZONTAL_WRITING = new Feature("horizontalWriting");
-    public static readonly RUBY_ANNOTATIONS = new Feature("rubyAnnotations");
-    public static readonly VERTICAL_WRITING = new Feature("verticalWriting");
-    public static readonly WITH_ADDITIONAL_WORD_SEGMENTATION = new Feature("withAdditionalWordSegmentation");
-    public static readonly WITHOUT_ADDITIONAL_WORD_SEGMENTATION = new Feature("withoutAdditionalWordSegmentation");
+    public static readonly NONE = new Feature('none');
+    public static readonly ANNOTATIONS = new Feature('annotations');
+    public static readonly ARIA = new Feature('ARIA');
+    public static readonly INDEX = new Feature('index');
+    public static readonly PAGE_BREAK_MARKERS = new Feature('pageBreakMarkers');
+    public static readonly PAGE_NAVIGATION = new Feature('pageNavigation');
+    public static readonly PRINT_PAGE_NUMBERS = new Feature('printPageNumbers');
+    public static readonly READING_ORDER = new Feature('readingOrder');
+    public static readonly STRUCTURAL_NAVIGATION = new Feature('structuralNavigation');
+    public static readonly TABLE_OF_CONTENTS = new Feature('tableOfContents');
+    public static readonly TAGGED_PDF = new Feature('taggedPDF');
+    public static readonly ALTERNATIVE_TEXT = new Feature('alternativeText');
+    public static readonly AUDIO_DESCRIPTION = new Feature('audioDescription');
+    public static readonly CAPTIONS = new Feature('captions');
+    public static readonly CLOSED_CAPTIONS = new Feature('closedCaptions');
+    public static readonly DESCRIBED_MATH = new Feature('describedMath');
+    public static readonly LONG_DESCRIPTION = new Feature('longDescription');
+    public static readonly OPEN_CAPTIONS = new Feature('openCaptions');
+    public static readonly SIGN_LANGUAGE = new Feature('signLanguage');
+    public static readonly TRANSCRIPT = new Feature('transcript');
+    public static readonly DISPLAY_TRANSFORMABILITY = new Feature('displayTransformability');
+    public static readonly SYNCHRONIZED_AUDIO_TEXT = new Feature('synchronizedAudioText');
+    public static readonly TIMING_CONTROL = new Feature('timingControl');
+    public static readonly UNLOCKED = new Feature('unlocked');
+    public static readonly CHEM_ML = new Feature('ChemML');
+    public static readonly LATEX = new Feature('latex');
+    public static readonly LATEX_CHEMISTRY = new Feature('latex-chemistry');
+    public static readonly MATH_ML = new Feature('MathML');
+    public static readonly MATH_ML_CHEMISTRY = new Feature('MathML-chemistry');
+    public static readonly TTS_MARKUP = new Feature('ttsMarkup');
+    public static readonly HIGH_CONTRAST_AUDIO = new Feature('highContrastAudio');
+    public static readonly HIGH_CONTRAST_DISPLAY = new Feature('highContrastDisplay');
+    public static readonly LARGE_PRINT = new Feature('largePrint');
+    public static readonly BRAILLE = new Feature('braille');
+    public static readonly TACTILE_GRAPHIC = new Feature('tactileGraphic');
+    public static readonly TACTILE_OBJECT = new Feature('tactileObject');
+    public static readonly FULL_RUBY_ANNOTATIONS = new Feature('fullRubyAnnotations');
+    public static readonly HORIZONTAL_WRITING = new Feature('horizontalWriting');
+    public static readonly RUBY_ANNOTATIONS = new Feature('rubyAnnotations');
+    public static readonly VERTICAL_WRITING = new Feature('verticalWriting');
+    public static readonly WITH_ADDITIONAL_WORD_SEGMENTATION = new Feature('withAdditionalWordSegmentation');
+    public static readonly WITHOUT_ADDITIONAL_WORD_SEGMENTATION = new Feature('withoutAdditionalWordSegmentation');
 }
 
 export class Hazard {
@@ -413,17 +413,17 @@ export class Hazard {
         return this.value;
     }
 
-    public static readonly FLASHING = new Hazard("flashing");
-    public static readonly NO_FLASHING_HAZARD = new Hazard("noFlashingHazard");
-    public static readonly UNKNOWN_FLASHING_HAZARD = new Hazard("unknownFlashingHazard");
-    public static readonly MOTION_SIMULATION = new Hazard("motionSimulation");
-    public static readonly NO_MOTION_SIMULATION_HAZARD = new Hazard("noMotionSimulationHazard");
-    public static readonly UNKNOWN_MOTION_SIMULATION_HAZARD = new Hazard("unknownMotionSimulationHazard");
-    public static readonly SOUND = new Hazard("sound");
-    public static readonly NO_SOUND_HAZARD = new Hazard("noSoundHazard");
-    public static readonly UNKNOWN_SOUND_HAZARD = new Hazard("unknownSoundHazard");
-    public static readonly UNKNOWN = new Hazard("unknown");
-    public static readonly NONE = new Hazard("none");
+    public static readonly FLASHING = new Hazard('flashing');
+    public static readonly NO_FLASHING_HAZARD = new Hazard('noFlashingHazard');
+    public static readonly UNKNOWN_FLASHING_HAZARD = new Hazard('unknownFlashingHazard');
+    public static readonly MOTION_SIMULATION = new Hazard('motionSimulation');
+    public static readonly NO_MOTION_SIMULATION_HAZARD = new Hazard('noMotionSimulationHazard');
+    public static readonly UNKNOWN_MOTION_SIMULATION_HAZARD = new Hazard('unknownMotionSimulationHazard');
+    public static readonly SOUND = new Hazard('sound');
+    public static readonly NO_SOUND_HAZARD = new Hazard('noSoundHazard');
+    public static readonly UNKNOWN_SOUND_HAZARD = new Hazard('unknownSoundHazard');
+    public static readonly UNKNOWN = new Hazard('unknown');
+    public static readonly NONE = new Hazard('none');
 }
 
 export class Exemption {
@@ -442,15 +442,15 @@ export class Exemption {
         return this.value;
     }
 
-    public static readonly NONE = new Exemption("none");
-    public static readonly DOCUMENTED = new Exemption("documented");
-    public static readonly LEGAL = new Exemption("legal");
-    public static readonly TEMPORARY = new Exemption("temporary");
-    public static readonly TECHNICAL = new Exemption("technical");
+    public static readonly NONE = new Exemption('none');
+    public static readonly DOCUMENTED = new Exemption('documented');
+    public static readonly LEGAL = new Exemption('legal');
+    public static readonly TEMPORARY = new Exemption('temporary');
+    public static readonly TECHNICAL = new Exemption('technical');
 
-    public static readonly EAA_DISPROPORTIONATE_BURDEN = new Exemption("eaa-disproportionate-burden");
-    public static readonly EAA_FUNDAMENTAL_ALTERATION = new Exemption("eaa-fundamental-alteration");
-    public static readonly EAA_MICROENTERPRISE = new Exemption("eaa-microenterprise");
-    public static readonly EAA_TECHNICAL_IMPOSSIBILITY = new Exemption("eaa-technical-impossibility");
-    public static readonly EAA_TEMPORARY = new Exemption("eaa-temporary");
+    public static readonly EAA_DISPROPORTIONATE_BURDEN = new Exemption('eaa-disproportionate-burden');
+    public static readonly EAA_FUNDAMENTAL_ALTERATION = new Exemption('eaa-fundamental-alteration');
+    public static readonly EAA_MICROENTERPRISE = new Exemption('eaa-microenterprise');
+    public static readonly EAA_TECHNICAL_IMPOSSIBILITY = new Exemption('eaa-technical-impossibility');
+    public static readonly EAA_TEMPORARY = new Exemption('eaa-temporary');
 }
