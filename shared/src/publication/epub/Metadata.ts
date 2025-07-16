9b0f732251fa3f13400c35935f677c62d7d65263
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2025-07-16 09:58:52 +0200

    Make copyright of additions consistent

--- shared/src/publication/epub/Metadata.ts
+++ shared/src/publication/epub/Metadata.ts
@@ -1,8 +1,3 @@
-/* Copyright 2025 Readium Foundation. All rights reserved.
- * Use of this source code is governed by a BSD-style license,
- * available in the LICENSE file present in the Github repository of the project.
- */
-
 import { Metadata } from "../Metadata";
 import { MediaOverlay } from "./MediaOverlay";
 
--- shared/test/epub/Metadata.test.ts
+++ shared/test/epub/Metadata.test.ts
@@ -1,8 +1,3 @@
-/* Copyright 2025 Readium Foundation. All rights reserved.
- * Use of this source code is governed by a BSD-style license,
- * available in the LICENSE file present in the Github repository of the project.
- */
-
 import { LocalizedString, MediaOverlay, Metadata } from '../../src';
 
 describe('EPUB Metadata Tests', () => {
