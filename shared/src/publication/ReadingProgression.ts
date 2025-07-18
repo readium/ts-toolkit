12a94f10916f63ce85c742d304561ca7d2f9bf56
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2025-07-18 09:12:08 +0200

    Add back leadingPage

--- shared/src/publication/ReadingProgression.ts
+++ shared/src/publication/ReadingProgression.ts
@@ -3,10 +3,26 @@
  * available in the LICENSE file present in the Github repository of the project.
  */
 
+import { Page } from "./Properties";
+
 export enum ReadingProgression {
   auto = 'auto',
   btt = 'btt',
   ltr = 'ltr',
   rtl = 'rtl',
   ttb = 'ttb',
+}
+
+export function leadingPage(readingProgression: ReadingProgression): Page {
+  switch (readingProgression) {
+    case ReadingProgression.auto:
+    case ReadingProgression.ttb:
+    case ReadingProgression.ltr:
+      return Page.left;
+    case ReadingProgression.rtl:
+    case ReadingProgression.btt:
+      return Page.right;
+    default:
+      return Page.left;
+  }
 }
