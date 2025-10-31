d4966f6082c16b31adac527d375415024fa968e9
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2025-10-31 13:04:42 +0100

    Update ReadiumCSS

--- navigator/src/webpub/css/WebPubStylesheet.ts
+++ navigator/src/webpub/css/WebPubStylesheet.ts
@@ -1,26 +1,21 @@
 // WebPubCSS is equivalent to ReadiumCSS for WebPub
+// Respects the stylesheet order from after
 
 export const webPubStylesheet = `
-/* FontFamily */
+/* TextAlign */
 
-:root[style*="--USER__fontFamily"] {
-  font-family: var(--USER__fontFamily) !important;
+:root[style*="--USER__textAlign"] {
+  text-align: var(--USER__textAlign);
 }
 
-:root[style*="--USER__fontFamily"] * {
-  font-family: revert !important;
-}
-
-/* FontWeight */
-
-:root[style*="--USER__fontWeight"] body {
-  font-weight: var(--USER__fontWeight) !important;
-}
-
-/* Attempt to handle known bolds */
-:root[style*="--USER__fontWeight"] b,
-:root[style*="--USER__fontWeight"] strong {
-  font-weight: bolder;
+:root[style*="--USER__textAlign"] body,
+:root[style*="--USER__textAlign"] p:not([class*="title"]):not(blockquote p):not(figcaption p):not(header p):not(hgroup p):not(div:has(+ *) > h1 + p):not(div:has(+ *) > p:has(+ h1)),
+:root[style*="--USER__textAlign"] li,
+:root[style*="--USER__textAlign"] dd {
+  text-align: var(--USER__textAlign) !important;
+  -moz-text-align-last: auto !important;
+  -epub-text-align-last: auto !important;
+  text-align-last: auto !important;
 }
 
 /* Hyphens */
@@ -45,84 +40,14 @@ export const webPubStylesheet = `
   hyphens: inherit;
 }
 
-/* LetterSpacing */
+/* FontFamily */
 
-:root[style*="--USER__letterSpacing"] h1,
-:root[style*="--USER__letterSpacing"] h2,
-:root[style*="--USER__letterSpacing"] h3,
-:root[style*="--USER__letterSpacing"] h4,
-:root[style*="--USER__letterSpacing"] h5,
-:root[style*="--USER__letterSpacing"] h6,
-:root[style*="--USER__letterSpacing"] p,
-:root[style*="--USER__letterSpacing"] li,
-:root[style*="--USER__letterSpacing"] div,
-:root[style*="--USER__letterSpacing"] dt,
-:root[style*="--USER__letterSpacing"] dd {
-  letter-spacing: var(--USER__letterSpacing);
-  font-variant: none;
+:root[style*="--USER__fontFamily"] {
+  font-family: var(--USER__fontFamily) !important;
 }
 
-/* Ligatures */
-
-:root[style*="--USER__ligatures"] {
-  font-variant-ligatures: var(--USER__ligatures) !important;
-}
-
-:root[style*="--USER__ligatures"] * {
-  font-variant-ligatures: inherit !important;
-}
-
-/* LineHeight */
-
-:root[style*="--USER__lineHeight"] {
-  line-height: var(--USER__lineHeight) !important;
-}
-
-:root[style*="--USER__lineHeight"] body,
-:root[style*="--USER__lineHeight"] p,
-:root[style*="--USER__lineHeight"] li,
-:root[style*="--USER__lineHeight"] div {
-  line-height: inherit;
-}
-
-/* ParagraphIndent */
-
-:root[style*="--USER__paraIndent"] p {
-  text-indent: var(--USER__paraIndent) !important;
-}
-
-:root[style*="--USER__paraIndent"] p *,
-:root[style*="--USER__paraIndent"] p:first-letter {
-  text-indent: 0 !important;
-}
-
-/* ParagraphSpacing */
-
-:root[style*="--USER__paraSpacing"] p {
-  margin-block: var(--USER__paraSpacing) !important;
-}
-
-/* Ruby */
-
-:root[style*="readium-noRuby-on"] body rt,
-:root[style*="readium-noRuby-on"] body rp {
-  display: none;
-}
-
-/* TextAlign */
-
-:root[style*="--USER__textAlign"] {
-  text-align: var(--USER__textAlign);
-}
-
-:root[style*="--USER__textAlign"] body,
-:root[style*="--USER__textAlign"] p:not(blockquote p):not(figcaption p):not(hgroup p),
-:root[style*="--USER__textAlign"] li,
-:root[style*="--USER__textAlign"] dd {
-  text-align: var(--USER__textAlign) !important;
-  -moz-text-align-last: auto !important;
-  -epub-text-align-last: auto !important;
-  text-align-last: auto !important;
+:root[style*="--USER__fontFamily"] * {
+  font-family: revert !important;
 }
 
 /* TextNormalize */
@@ -132,13 +57,13 @@ export const webPubStylesheet = `
   font-style: normal !important;
 }
 
-:root[style*="readium-a11y-on"] *:not(code):not(var):not(kbd):not(samp) {
+:root[style*="readium-a11y-on"] body *:not(code):not(var):not(kbd):not(samp) {
   font-family: inherit !important;
   font-weight: inherit !important;
   font-style: inherit !important;
 }
 
-:root[style*="readium-a11y-on"] * {
+:root[style*="readium-a11y-on"] body * {
   text-decoration: none !important;
   font-variant-caps: normal !important;
   font-variant-position: normal !important;
@@ -149,22 +74,6 @@ export const webPubStylesheet = `
 :root[style*="readium-a11y-on"] sub {
   font-size: 1rem !important;
   vertical-align: baseline !important;
-}
-
-/* Word Spacing */
-
-:root[style*="--USER__wordSpacing"] h1,
-:root[style*="--USER__wordSpacing"] h2,
-:root[style*="--USER__wordSpacing"] h3,
-:root[style*="--USER__wordSpacing"] h4,
-:root[style*="--USER__wordSpacing"] h5,
-:root[style*="--USER__wordSpacing"] h6,
-:root[style*="--USER__wordSpacing"] p,
-:root[style*="--USER__wordSpacing"] li,
-:root[style*="--USER__wordSpacing"] div,
-:root[style*="--USER__wordSpacing"] dt,
-:root[style*="--USER__wordSpacing"] dd {
-  word-spacing: var(--USER__wordSpacing);
 }
 
 /* Zoom */
@@ -200,5 +109,97 @@ export const webPubStylesheet = `
   :root[style*="--USER__zoom"] th {
     zoom: var(--USER__zoom) !important;
   }
+}
+
+/* LineHeight */
+
+:root[style*="--USER__lineHeight"] {
+  line-height: var(--USER__lineHeight) !important;
+}
+
+:root[style*="--USER__lineHeight"] body,
+:root[style*="--USER__lineHeight"] p,
+:root[style*="--USER__lineHeight"] li,
+:root[style*="--USER__lineHeight"] div {
+  line-height: inherit;
+}
+
+/* ParagraphSpacing */
+
+:root[style*="--USER__paraSpacing"] p {
+  margin-block: var(--USER__paraSpacing) !important;
+}
+
+/* ParagraphIndent */
+
+:root[style*="--USER__paraIndent"] p {
+  text-indent: var(--USER__paraIndent) !important;
+}
+
+:root[style*="--USER__paraIndent"] p *,
+:root[style*="--USER__paraIndent"] p:first-letter {
+  text-indent: 0 !important;
+}
+
+/* WordSpacing */
+
+:root[style*="--USER__wordSpacing"] h1,
+:root[style*="--USER__wordSpacing"] h2,
+:root[style*="--USER__wordSpacing"] h3,
+:root[style*="--USER__wordSpacing"] h4,
+:root[style*="--USER__wordSpacing"] h5,
+:root[style*="--USER__wordSpacing"] h6,
+:root[style*="--USER__wordSpacing"] p,
+:root[style*="--USER__wordSpacing"] li,
+:root[style*="--USER__wordSpacing"] div,
+:root[style*="--USER__wordSpacing"] dt,
+:root[style*="--USER__wordSpacing"] dd {
+  word-spacing: var(--USER__wordSpacing);
+}
+
+/* LetterSpacing */
+
+:root[style*="--USER__letterSpacing"] h1,
+:root[style*="--USER__letterSpacing"] h2,
+:root[style*="--USER__letterSpacing"] h3,
+:root[style*="--USER__letterSpacing"] h4,
+:root[style*="--USER__letterSpacing"] h5,
+:root[style*="--USER__letterSpacing"] h6,
+:root[style*="--USER__letterSpacing"] p,
+:root[style*="--USER__letterSpacing"] li,
+:root[style*="--USER__letterSpacing"] div,
+:root[style*="--USER__letterSpacing"] dt,
+:root[style*="--USER__letterSpacing"] dd {
+  letter-spacing: var(--USER__letterSpacing);
+  font-variant: none;
+}
+
+/* FontWeight */
+
+:root[style*="--USER__fontWeight"] body {
+  font-weight: var(--USER__fontWeight) !important;
+}
+
+/* Attempt to handle known bolds */
+:root[style*="--USER__fontWeight"] b,
+:root[style*="--USER__fontWeight"] strong {
+  font-weight: bolder;
+}
+
+/* Ruby */
+
+:root[style*="readium-noRuby-on"] body rt,
+:root[style*="readium-noRuby-on"] body rp {
+  display: none;
+}
+
+/* Ligatures */
+
+:root[style*="--USER__ligatures"] {
+  font-variant-ligatures: var(--USER__ligatures) !important;
+}
+
+:root[style*="--USER__ligatures"] * {
+  font-variant-ligatures: inherit !important;
 }
 `;
