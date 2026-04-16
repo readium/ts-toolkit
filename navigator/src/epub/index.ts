e358b3fb346b43fe129d23a7edfa6d97c1ef83fa
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2026-04-16 15:02:54 +0200

    Expose getScriptMode

--- navigator/src/epub/index.ts
+++ navigator/src/epub/index.ts
@@ -3,3 +3,4 @@ export * from "./frame/index.ts";
 export * from "./fxl/index.ts";
 export * from "./preferences/index.ts";
 export * from "./css/index.ts";
+export { getScriptMode, type ScriptMode } from "./helpers/scriptMode.ts";
