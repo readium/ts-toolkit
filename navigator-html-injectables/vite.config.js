776b1af7b348c54bc50e29d78f99ccba6926929e
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2026-05-28 15:30:00 +0200

    fix dist names

--- navigator-html-injectables/vite.config.js
+++ navigator-html-injectables/vite.config.js
@@ -4,15 +4,16 @@ import packageJson from "./package.json";
 
 export default defineConfig({
   build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      formats: ["es"],
-    },
     rollupOptions: {
+      input: resolve(__dirname, "src/index.ts"),
       external: ["@readium/shared"],
+      preserveEntrySignatures: "strict",
       output: {
+        format: "es",
         preserveModules: true,
-        preserveModulesRoot: "src",
+        preserveModulesRoot: resolve(__dirname, "src"),
+        dir: "dist",
+        entryFileNames: "[name].js",
       }
     }
   },
--- shared/vite.config.js
+++ shared/vite.config.js
@@ -4,14 +4,15 @@ import packageJson from "./package.json";
 
 export default defineConfig({
   build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      formats: ["es"],
-    },
     rollupOptions: {
+      input: resolve(__dirname, "src/index.ts"),
+      preserveEntrySignatures: "strict",
       output: {
+        format: "es",
         preserveModules: true,
-        preserveModulesRoot: "src",
+        preserveModulesRoot: resolve(__dirname, "src"),
+        dir: "dist",
+        entryFileNames: "[name].js",
       }
     }
   },
